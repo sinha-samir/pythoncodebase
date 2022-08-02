885b8d22ed45abe1e13c277aecdbfd69366172cd
sinha-samir <110466450+sinha-samir@users.noreply.github.com>
2022-08-02 23:20:08 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ProjectUI',
+    version='1.0.0',
+    # packages=[''],
+    url='https://github.com/sinha-samir/pythoncodebase.git',
+    license='None',
+    author='samirsinha',
+    author_email='samsin.sinha@gmail.com',
+    description='Simplilearn Project UI',
+    packages=find_packages()
+)
